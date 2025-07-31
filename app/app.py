631857e7b600affd3ca15fd254d68019cc564d3b
bharat007-commits <Bharatkaushik174@example.com>
2025-07-31 20:20:29 +0530

    abc

--- app/app.py
+++ app/app.py
@@ -1,16 +1,17 @@
-import streamlit as st
+import streamlit as st 
 from pipeline.pipeline import AnimeRecommendationPipeline
 from dotenv import load_dotenv
 
-st.set_page_config(page_title="Anime Recommnder",layout="wide")
 
+
+st.set_page_config(page_title="Anime Recommender",layout="wide")
 load_dotenv()
 
 @st.cache_resource
 def init_pipeline():
     return AnimeRecommendationPipeline()
 
-pipeline = init_pipeline()
+pipeline =init_pipeline()
 
 st.title("Anime Recommender System")
 
@@ -20,5 +21,6 @@ if query:
         response = pipeline.recommend(query)
         st.markdown("### Recommendations")
         st.write(response)
+
 
 
